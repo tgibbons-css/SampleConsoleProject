94f07098ac5ee5fe0dc86966996d25021388f575
tgibbons-css <tgibbons@css.edu>
2024-09-17 10:39:24 -0500

    updated commetn

--- SampleConsoleProject/Program.cs
+++ SampleConsoleProject/Program.cs
@@ -1,4 +1,4 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, World!");
 Console.WriteLine("Adding a line in Visual Studio");
-// added this line
+// added this line -- added to comment
